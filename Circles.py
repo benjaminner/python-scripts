181fa3ec7082fd99509b5cf79a2908ff753028cc
Benjamin Mangiafico <benmangiafico@gmail.com>
2019-05-16 19:44:54 -0700

    now with area

--- Circles.py
+++ Circles.py
@@ -1,45 +1,64 @@
-Input = raw_input("Enter the kind of dimension you would like to enter: ")
-Output = raw_input("Enter what you would like to get back:") 
-if Input == "Radius" or "radius" :
+pi = 3.14159265358
+import math
+Input = raw_input('Enter the kind of dimension you would like to enter( "Circumference", "Diameter" , "Area" , "Radius" ): ')
+Output = raw_input('Enter what you would like to get back( "Circumference", "Diameter" , "Area" , "Radius" ): ') 
+if Input in ["Radius" , "radius"] :
      Radius = raw_input("Enter the radius of a circle: ")
      
-     if Output == "Circumference" or "circumference" :
-             Circumference = int(Radius) * 2 * 3.14159265358 
-             print("The circumference of your circle is: ")
+     if Output in ["Circumference" , "circumference"] :
+             Circumference = int(Radius) * 2 * pi 
+             print("The circumference of your circle is:")
              print(Circumference)
              
-     if Output == "Diameter" or "diameter" :
+     if Output in ["Diameter" , "diameter"] :
              Diameter = int(Radius) * 2 
-             print("The diameter of your circle is: ")
+             print("The diameter of your circle is:")
              print(Diameter)
      
-     if Output == "Area" or "area" :
-             Area = (int(Radius) * int(Radius)) * 3.14159265358 
+     if Output in ["Area" , "area"] :
+             Area = (int(Radius) **2) * pi
              print("The area of your circle is:")
              print(Area)
 
-if Input == "Diameter" or "diameter" :
-     Diameter = raw_input("Enter the radius of a circle: ")
+if Input in ["Diameter" , "diameter"] :
+     Diameter = raw_input("Enter the diameter of a circle: ")
      
-     if Output == "Circumference" or "circumference" :
-             Circumference = int(Diameter) * 3.14159265358 
-             print("The circumference of your circle is: ")
+     if Output in ["Circumference" , "circumference"] :
+             Circumference = int(Diameter) * pi 
+             print("The circumference of your circle is:")
              print(Circumference)
              
-     if Output == "Radius" or "radius" :
+     if Output in ["Radius" , "radius"] :
              Radius = int(Diameter) / 2 
-             print("The Radius of your circle is: ")
+             print("The Radius of your circle is:")
              print(Radius)
      
-     if Output == "Area" or "area" :
-             Area = ((int(Diameter) / 2) * (int(Diameter) / 2)) * 3.14159265358 
+     if Output in ["Area" , "area"] :
+             Area = ((int(Diameter) / 2) **2) * pi 
              print("The area of your circle is:")
              print(Area)
+             
+if Input in ["Area" , "area"] :
+    Area = raw_input("Enter the area of a circle: ")
+    
+    if Output in ["Circumference" , "circumference"] :
+            Circumference = math.sqrt( int(Area) / pi) * 2 * pi
+            print("The circumference of your circle is:")
+            print(Circumference)
+            
+    if Output in ["Diameter" , "diameter"] :
+            Diameter = math.sqrt(int(Area) / pi) * 2
+            print("The diameter of your circle is:")
+            print(Diameter)
+    
+    if Output in ["Radius" , "radius"] :
+            Radius = math.sqrt(int(Area) / pi)
+            print("The area of your circle is:")
+            print(Radius)
 
+print("Thank you for using Circles.py!")
 
-
-
-"Circumference" or "circumference" 
-"Diameter" or "diameter"  
-"Area" or "area"
-"Radius" or "radius" 
+#[ "Circumference" , "circumference" ] 
+#[ "Diameter" , "diameter" ] 
+#[ "Area" , "area" ]
+#[ "Radius" , "radius" ]
