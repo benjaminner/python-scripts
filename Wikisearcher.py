9a645522b91fb19a65c102f3f40c8e8272ac377f
Benjamin Mangiafico <benmangiafico@gmail.com>
2019-06-01 10:44:00 -0700

    Cool way to Wikipedia

--- Wikisearcher.py
+++ Wikisearcher.py
@@ -1,11 +1,8 @@
 x = 0 
 
-for x = 0 : 
+for x in [ "0" ] : 
     import webbrowser 
-    WikiSearch = raw_input("Search the wiki!") 
+    WikiSearch = raw_input("Search the wiki: ") 
 
     webbrowser.open_new_tab("https://www.wikipedia.org/wiki/" + WikiSearch)
     
-    Go_Again = raw_input("Press enter if you would like to go again! ")
-    if Go_Again != "" :
-        x = 1
