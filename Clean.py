790da0ee69c1f69ddcfb16bb6f3246898cb636df
Benjamin Mangiafico <benmangiafico@gmail.com>
2019-10-06 20:37:10 -0700

    just a reminder

--- Clean.py
+++ Clean.py
@@ -5,3 +5,7 @@ message = raw_input("What would you like your commit message to be? ")
 os.system("git add .")
 os.system("git commit -m " + '"' + message + '"')
 os.system("git push")
+
+
+# cd
+# ~/.bash_profile
