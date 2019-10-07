109ba37454dcd367ba057f39b03236afb227f209
Benjamin Mangiafico <benmangiafico@gmail.com>
2019-10-06 20:55:25 -0700

    more user friendly

--- Clean.py
+++ Clean.py
@@ -2,6 +2,8 @@ import os
 
 message = raw_input("What would you like your commit message to be? ")
 
+os.system("git status")
+print("#############The files above will be updated############")
 os.system("git add .")
 os.system("git commit -m " + '"' + message + '"')
 os.system("git push")
