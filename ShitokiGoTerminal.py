6c490bbcf5eb6f37cb08be75f594864120c6a050
Benjamin Mangiafico <benmangiafico@gmail.com>
2019-10-20 13:36:49 -0700

    updating terminal!!!

--- ShitokiGoTerminal.py
+++ ShitokiGoTerminal.py
@@ -1,18 +1,22 @@
 print('Hello, and welcome to the Shitoki Go! interactive terminal(SGIT)!')
 import os 
-
+import webbrowser
 
 while True:
     query = raw_input(">>> ")
     if query[:3] == "lgn" :
         login = query[3:]
         terminR = open("terminalfile.txt","a")
-        termin = open("terminalfile.txt","r")
-        terminR.write(login)   
+        terminR.write(login)
+        termin = open("terminalfile.txt","r")   
         print(termin.read())
         
-    if query[:1] == "." :
-        if query[1:11] == "action.do." :
+    if query[:8] == ".action." :
+        if query[8:11] == "do." :
             action = query[11:]
+            webbrowser.open("https://www.youtube.com/results?search_query=" + action)
+        if query[8:14] == "speak." :
+            saY = query[14:]
+            os.system("say " + saY)
     if query == "quit()" :
             break
