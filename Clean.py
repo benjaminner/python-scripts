269824313f0ad1314e86166d351ea62d9b66be9f
Ben <ben@Karens-MacBook-Air-5.local>
2020-07-22 11:33:59 -0700

    Just a check.

--- Clean.py
+++ Clean.py
@@ -4,9 +4,11 @@ message = raw_input("What would you like your commit message to be? ")
 
 os.system("git status")
 print("#############The files above in red will be updated#############")
-os.system("git add .")
-os.system("git commit -m " + '"' + message + '"')
-os.system("git push")
+check = raw_input("Are you sure?(y/n) ")
+if check.lower() == "y":
+    os.system("git add .")
+    os.system("git commit -m " + '"' + message + '"')
+    os.system("git push")
 
 
 # cd
